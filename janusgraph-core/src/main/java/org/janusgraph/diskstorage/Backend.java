fc02407e75feb0da6c4d3f081a20ae5749922c2f
Austin Sharp <austin.sharp@seeq.com>
2018-02-13 16:11:03 -0800

    Disable all external indices so an ES-backed graph can boot without ES

--- janusgraph-core/src/main/java/org/janusgraph/diskstorage/Backend.java
+++ janusgraph-core/src/main/java/org/janusgraph/diskstorage/Backend.java
@@ -457,16 +457,19 @@ public class Backend implements LockerProvider, AutoCloseable {
     }
 
     private final static Map<String, IndexProvider> getIndexes(Configuration config) {
-        ImmutableMap.Builder<String, IndexProvider> builder = ImmutableMap.builder();
-        for (String index : config.getContainedNamespaces(INDEX_NS)) {
-            Preconditions.checkArgument(StringUtils.isNotBlank(index), "Invalid index name [%s]", index);
-            log.info("Configuring index [{}]", index);
-            IndexProvider provider = getImplementationClass(config.restrictTo(index), config.get(INDEX_BACKEND,index),
-                    StandardIndexProvider.getAllProviderClasses());
-            Preconditions.checkNotNull(provider);
-            builder.put(index, provider);
-        }
-        return builder.build();
+        // Because it's impossible to disable the ES index otherwise.
+        return ImmutableMap.of();
+//
+//        ImmutableMap.Builder<String, IndexProvider> builder = ImmutableMap.builder();
+//        for (String index : config.getContainedNamespaces(INDEX_NS)) {
+//            Preconditions.checkArgument(StringUtils.isNotBlank(index), "Invalid index name [%s]", index);
+//            log.info("Configuring index [{}]", index);
+//            IndexProvider provider = getImplementationClass(config.restrictTo(index), config.get(INDEX_BACKEND,index),
+//                    StandardIndexProvider.getAllProviderClasses());
+//            Preconditions.checkNotNull(provider);
+//            builder.put(index, provider);
+//        }
+//        return builder.build();
     }
 
     public final static <T> T getImplementationClass(Configuration config, String clazzname, Map<String, String> registeredImpls) {
